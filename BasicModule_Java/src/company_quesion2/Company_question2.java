47cec36fb8a1a7c471cba1b3be4709969262928a
USER_2 <USER_2@USER-PC>
2019-09-20 19:29:46 -0700

    Company Question 2

---
+++ BasicModule_Java/src/company_quesion2/Company_question2.java
@@ -0,0 +1,37 @@
+package company_quesion2;
+
+public class Company_question2
+{
+
+	public static void main(String[] args) 
+	{
+		String s = "WELCOME";
+		int v=0,c=0;;
+		for(int i=0;i<s.length();i++)
+		{
+		char ch = s.charAt(i);
+		if(ch == 'A' || ch == 'E' || ch == 'I'
+		               || ch == 'O' || ch == 'U') {
+
+		              v++;
+		              for(int j=0;j<v;j++)
+		              {
+
+		  System.out.print("*");
+		              }
+		              System.out.print(ch);
+		              for(int j=0;j<v;j++)
+		              {
+		  System.out.print("*");
+		              }
+		           
+		             
+		           
+		           }
+		else
+
+		System.out.print(ch);
+	
+		}
+	}
+}
