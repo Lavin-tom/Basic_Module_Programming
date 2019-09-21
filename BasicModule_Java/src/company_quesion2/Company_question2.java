fe13cd2c221cd5f6514dba72ce907b0283f62983
USER_2 <USER_2@USER-PC>
2019-09-20 19:36:09 -0700

    Company Question 2

--- BasicModule_Java/src/company_quesion2/Company_question2.java
+++ BasicModule_Java/src/company_quesion2/Company_question2.java
@@ -1,37 +1,51 @@
 package company_quesion2;
 
+import java.util.Scanner;
+
 public class Company_question2
 {
 
 	public static void main(String[] args) 
 	{
-		String s = "WELCOME";
-		int v=0,c=0;;
-		for(int i=0;i<s.length();i++)
-		{
-		char ch = s.charAt(i);
-		if(ch == 'A' || ch == 'E' || ch == 'I'
-		               || ch == 'O' || ch == 'U') {
+		Scanner sc=new Scanner(System.in);
+		 
+		 
 
-		              v++;
-		              for(int j=0;j<v;j++)
-		              {
+		 
+	       System.out.println("enter the  String :");
+	       
+	      String str= sc.next();
+	 
+	      int v=0,c=0;;
+	      for(int i=0;i<str.length();i++)
+	      {
+	      char ch = str.charAt(i);
+	      if(ch == 'A' || ch == 'E' || ch == 'I'
+	                     || ch == 'O' || ch == 'U') {
 
-		  System.out.print("*");
-		              }
-		              System.out.print(ch);
-		              for(int j=0;j<v;j++)
-		              {
-		  System.out.print("*");
-		              }
-		           
-		             
-		           
-		           }
-		else
+	                    v++;
+	                    for(int j=0;j<v;j++)
+	                    {
 
-		System.out.print(ch);
-	
-		}
+	        System.out.print("*");
+	                    }
+	                    System.out.print(ch);
+	                    for(int j=0;j<v;j++)
+	                    {
+	        System.out.print("*");
+	                    }
+	                 
+	                   
+	                 
+	                 }
+	      else
+	      {
+
+	      System.out.print(ch);
+	      //c++;
+	      }
+	      }
+
 	}
-}
+
+	}
